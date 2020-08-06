82dc62f0229b47bdb5187f6f2a3cd412b6cf6231
vqyj <66005097+vqyj@users.noreply.github.com>
2020-08-06 16:17:47 +0800

    Update sketch.ver4.js

--- TEST/sketch.ver4.js
+++ TEST/sketch.ver4.js
@@ -1,1 +1,31 @@
-let vig,status=0;function setup(){noCanvas();let t=select("#button");t.mousePressed(function(){0==status?(status=1,t.html("d")):1==status&&(status=0,t.html("e"));n()});let e=select("#input"),s=select("#pw");e.input(n),s.input(n);let l=select("#para");function n(){vig=vigenere(e.value(),s.value(),status),vig=vig.replace(/(?:\r\n|\r|\n)/g,"<br>");let t="";for(let e=0;e<vig.length;e++)" "==vig[e]?t+="&nbsp":t+=vig[e];l.html(t)}}
+let vig;
+let status = 0;
+function setup() {
+  noCanvas();
+  let but = select("#button");
+  but.mousePressed(changeStatus);
+
+  function changeStatus() {
+    if (status == 0) {
+      status = 1;
+      but.html("d");
+    } else if (status == 1) {
+      status = 0;
+      but.html("e");
+    }
+    changeParaHtml();
+  }
+  let textBox = select("#input")
+  let pw = select("#pw")
+  textBox.input(changeParaHtml);
+  pw.input(changeParaHtml);
+  let para = select('#para')
+
+
+  function changeParaHtml() {
+    vig = vigenere(textBox.value(), pw.value(), status);
+
+
+    para.html(vig);
+  }
+}
